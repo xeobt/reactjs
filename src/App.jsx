0dff90eab261264d519ee857eceea4f3a6dce0e3
xeobt <xeoinf@gmail.com>
2026-07-13 11:07:16 +0100

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 function App() {
-  const students = ["Rahim", "Karim", "Jamal", "Sakib", "Basan", "Rafil", "ghgdd"];
-
-  const showStudent = ["Rahim","Basan"]
+  const students = [
+    {
+      name: "Rahim",
+      age: 20,
+      city: "Dhaka"
+    },
+    {
+      name: "Karim",
+      age: 22,
+      city: "Chittagong"
+    },
+    {
+      name: "Jamal",
+      age: 21,
+      city: "Khulna"
+    }
+  ]
 
   return (
     <div>
-      {students.filter((student) => showStudent.includes(student))
-      .map((student, index) => (
-      <h2 key={index}>{student}</h2>
+      {students.map((student, index) => (
+      <div key={index}>
+        <h2>{student.name}</h2>
+        <p>Age: {student.age}</p>
+        <p>City: {student.city}</p>
+      </div>
       ))}
     </div>
   )
