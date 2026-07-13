7331eec15839558ebd7aa8b506022491c4532889
xeobt <xeoinf@gmail.com>
2026-07-13 09:59:00 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import Header from "./Header";
-import Hero from "./Hero";
-import Sidebar from "./Sidebar"
-import Footer from "./Footer"
-
 function App() {
+  const students = ["Rahim", "Karim", "Jamal"];
+
   return (
-    <div style={{backgroundImage: "linear-gradient(to bottom right, #062F5B, #084A76, #41A5B2, #5CB8BD, #82CDCF, #A9E0DE)"}}>
-      <Header 
-      title="Daily News"
-      nav1="Home"
-      nav2="Sports"
-      nav3="Technology"
-      nav4="Contact"
-      />
-
-      <Hero
-      headline="Breaking News"
-      description="A new AI technilogy is changing future of education."
-      />
-
-      <Sidebar
-      title="Latest News"
-      item1="Football World Cup"
-      item2="Stock Market Update"
-      item3="Weather Forecast"
-      />
-
-      <Footer
-      text="@ 2026 Daily News. All Rights Reserved."
-      />
+    <div>
+      {students.map((student) => (
+      <h2>{student}</h2>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
