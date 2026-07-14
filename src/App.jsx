f09a7ab12cb53c449c61881f78723ccc43778e85
xeobt <xeoinf@gmail.com>
2026-07-14 10:20:16 +0100

    Toggle between states with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [liked, setLiked] = useState(false);
 
   return (
-    <div>
-      <h1>{count}</h1>
-
-      <button style={{width: "50%", height: "70vh"}} onClick={() => setCount(count + 1)}>+</button>
-
-      <button style={{width: "50%", height: "70vh"}}  onClick={() => setCount(count - 1)}>-</button>
-
-      <button onClick={() => setCount(0)}>Reset</button>
-    </div>
+    <button onClick={() => setLiked(!liked)}>
+      {liked ? "Liked" : "Like"}
+    </button>
   );
 }
 
